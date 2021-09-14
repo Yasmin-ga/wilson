9c3b7506bcc8e6d02fe4bc187652c8279acb73c2
FredericoVinci <frederico@agenciavinci.com.br>
2021-09-14 15:21:52 -0300

    galeria slick

--- src/js/galeria.js
+++ src/js/galeria.js
@@ -1,6 +1,22 @@
-$('.slider').slick({
-    slidesToShow: 1,
-    slidesToScroll: 6,
-    arrows: false,
-    fade: true,
-});
+$(document).ready(()=>{
+   const imagemMaior = document.querySelector(".imagem-maior img")
+   const imageList = document.querySelectorAll(".slider li img")
+   
+   imageList.forEach(image => {
+       image.addEventListener("click", ()=>{
+           imagemMaior.src = image.src
+       })
+   })
+
+   
+    $('.slider').slick({
+        slidesToShow: 6,
+        slidesToScroll: 2,
+        arrows: true,
+        //prevArrow: 
+        //nextArrow: 
+    });
+
+
+
+})
